484044c11c0c14accac2c3afe4d3ab5a271b6ef6
Raul-A-Flores <raul.alexander.floress@gmail.com>
2024-05-30 16:30:38 -0700

    mongo

---
+++ src/slackbot/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+
+	os.Setenv("SLACK_BOT_TOKEN", "")
+	os.Setenv("CHANNEL_ID", "C076JNSGMUG")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"dehya.jpg"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+
+		}
+		fmt.Printf("Name: %s, URL:%s\n", file.Name, file.URL)
+	}
+
+}
