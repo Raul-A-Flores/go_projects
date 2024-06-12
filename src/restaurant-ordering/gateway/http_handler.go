4debfb0006804afe118727046366de50aff5a8aa
Raul-A-Flores <raul.alexander.floress@gmail.com>
2024-06-12 15:21:08 -0700

    grpc service enabled

---
+++ src/restaurant-ordering/gateway/http_handler.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"common"
+	pb "common/api"
+	"net/http"
+)
+
+type handler struct {
+
+	// gateway
+	client pb.OrderServiceClient
+}
+
+func NewHandler(client pb.OrderServiceClient) *handler {
+	return &handler{client}
+
+}
+
+func (h *handler) registerRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("POST /api/customers/{customerID}/orders", h.HandleCreateOrder)
+
+}
+
+func (h *handler) HandleCreateOrder(w http.ResponseWriter, r *http.Request) {
+
+	customerID := r.PathValue("customerID")
+
+	var items []*pb.ItemsWithQuantity
+	if err := common.ReadJSON(r, &items); err != nil {
+		common.WriteError(w, http.StatusBadGateway, err.Error())
+		return
+
+	}
+
+	h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
+		CustomerID: customerID,
+		Items:      items,
+	})
+}
---
+++ src/restaurant-ordering/orders/grpc_handler.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	pb "common/api"
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+type grpcHandler struct {
+	pb.UnimplementedOrderServiceServer
+}
+
+func NewGRPHandler(grpcServer *grpc.Server) {
+	handler := &grpcHandler{}
+	pb.RegisterOrderServiceServer(grpcServer, handler)
+
+}
+
+func (h *grpcHandler) CreateOrder(context.Context, *pb.CreateOrderRequest) (*pb.Order, error) {
+
+	log.Println("New order received!")
+
+	o := &pb.Order{
+		ID: "42",
+	}
+
+	return o, nil
+}
